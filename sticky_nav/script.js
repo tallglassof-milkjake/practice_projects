f68f54495d0c99e44b607c1f17fb42408071af69
Jake Mackinlay <jrmackinlay90@gmail.com>
2021-07-11 16:12:28 +0930

    footer?

--- sticky_nav/script.js
+++ sticky_nav/script.js
@@ -24,6 +24,7 @@ window.addEventListener('scroll', () => {
 
 const navMenu = document.getElementById('nav');
 const btns = document.getElementsByClassName('btn');
+const icons = document.getElementsByClassName('fab');
 
 for (var i = 0; i < btns.length; i++) {
     btns[i].addEventListener("click", function(e) {
@@ -35,6 +36,17 @@ for (var i = 0; i < btns.length; i++) {
       body.className.add('active')
     });
 }
+
+// for (var i = 0; i < icons.length; i++) {
+//     icons[i].addEventListener("click", function(e) {
+//         // e.preventDefault();
+//       var current = document.getElementsByClassName("active");
+//       current[0].className = current[0].className.replace(" active", "");
+//       this.className += " active";
+
+//       body.className.add('active')
+//     });
+// }
 
 const panels = document.querySelectorAll('.panel');
 
@@ -50,3 +62,21 @@ function removeActiveClasses() {
         panel.classList.remove('active');
     });
 };
+
+// form animations
+const labels = document.querySelectorAll('.form-control label');
+
+labels.forEach(label => {
+    label.innerHTML = label.innerText
+    .split('')
+    .map((letter, index) => `<span style="transition-delay:${index * 50}ms">${letter}</span>`)
+    .join('');
+});
+
+
+
+// document.querySelector('#text-area').focus(function() {
+//     document.querySelector(this).value = '';
+//  });
+
+document.querySelector('#text-area').blur();
